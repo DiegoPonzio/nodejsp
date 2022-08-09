55fad78d0144b605565ab15a110912dcb8fac5c9
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-08-09 13:37:30 -0500

    Debug

---
+++ debug/app.js
@@ -0,0 +1,13 @@
+let curso = "node";
+curso = "node.js";
+curso = "node.js pract";
+
+debugger;
+
+console.log(curso);
+
+x = () => {
+    return 15 + 45;
+}
+
+console.log(x());
---
+++ debug/extra.js
@@ -0,0 +1,9 @@
+let mensaje = "Debug the code";
+
+saludar = ()=> {
+    console.log("hola mundo");
+};
+
+module.exports = {
+    saludar : saludar()
+};
